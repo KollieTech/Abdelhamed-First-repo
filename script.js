5f5613ce946e365cb2fa57df13c6c9760ca45eae
Abdelhamed Elkoli <Abdel_elkoli@hotmail.com>
2026-09-24 04:37:57 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const rubrik = document.getElementById("rubrik");
+const knapp = document.getElementById("knapp");
+
+knapp.addEventListener("click", function(){
+    rubrik.textContent = "Goodbye World";
+});
